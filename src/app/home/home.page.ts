092509ca9e3c19e4490fcd4e1815ea915f5fbeb3
Kevin Cowdrey <kevin@gendac.co.za>
2020-05-20 11:57:50 +0200

    bla

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { InAppBrowser } from "@ionic-native/in-app-browser/ngx";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  constructor(private iab: InAppBrowser) {}
 
-  constructor() {}
-
+  openBrowser() {
+    this.iab.create(
+      "http://www.ijcat.com/archives/volume3/issue12/ijcatr03121001.pdf",
+      "_system",
+      "location=no"
+    );
+  }
 }
